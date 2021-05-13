40952c7add6d74e2982aac15de5f6f8e72bb520c
Raul Santos Iglesias <51235779+RaulIglesias@users.noreply.github.com>
2021-05-13 11:21:57 -0300

    Initial commit
    Fazendo upload do projeto para o github.

---
+++ CRUD-POO/src/main/java/com/BeyondLearning/projetointegrador/models/entity/Usuarios.java
@@ -0,0 +1,113 @@
+package com.BeyondLearning.projetointegrador.models.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "usuarios")
+public class Usuarios implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	
+	private Long id;
+	
+	@Column(name = "nome")
+	private String nome;
+	
+	@Column(name = "cpf")
+	private String cpf;
+	
+	@Column(name = "email")
+	private String email;
+	
+	
+	@Column(name = "nivel")
+	private String nivel;
+	
+	@Column(name = "telefone")
+	private String telefone;
+	
+		
+	public Long getId() {
+		return id;
+	}
+
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+
+	public String getNome() {
+		return nome;
+	}
+
+
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+
+
+	public String getCpf() {
+		return cpf;
+	}
+
+
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+
+	public String getNivel() {
+		return nivel;
+	}
+
+
+
+	public void setNivel(String nivel) {
+		this.nivel = nivel;
+	}
+
+
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	
+}
---
+++ CRUD-POO/src/main/java/com/BeyondLearning/projetointegrador/models/service/UsuariosServiceImpl.java
@@ -0,0 +1,60 @@
+package com.BeyondLearning.projetointegrador.models.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import com.BeyondLearning.projetointegrador.models.entity.Usuarios;
+import com.BeyondLearning.projetointegrador.models.repository.UsuariosRepository;
+
+@Service
+public class UsuariosServiceImpl implements IUsuariosService{
+
+	
+	@Autowired
+	private UsuariosRepository usuariosRepository;
+	
+	@Override
+	public List<Usuarios> getAllUsuarios() {
+		return usuariosRepository.findAll();
+	}
+
+	@Override
+	public void saveUsuarios(Usuarios usuarios) {
+		this.usuariosRepository.save(usuarios);
+		
+	}
+
+	@Override
+	public Usuarios getUsuariosById(long id) {
+		Optional<Usuarios> optional = usuariosRepository.findById(id);
+		Usuarios usuarios = null;
+		if(optional.isPresent()) {
+			usuarios = optional.get();
+		}else {
+			throw new RuntimeException(" Usuário não Encontrado pelo ID :: "+ id);
+		}
+		return usuarios;
+	}
+
+	@Override
+	public void deleteUsuariosById(long id) {
+		
+		this.usuariosRepository.deleteById(id);
+		
+	}
+
+	@Override
+	public Page<Usuarios> findPaginated(int pageNo, int pageSize) {
+		
+		
+		Pageable pageable = PageRequest.of(pageNo - 1, pageSize);
+		return this.usuariosRepository.findAll(pageable);
+	}
+
+}
